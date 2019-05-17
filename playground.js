9d80ccc81d27fdecbe15a678f09675765ee1a905
Isaque Dias <isaquediasm@gmail.com>
2019-05-17 13:37:53 +0200

    commit

---
+++ playground.js
@@ -0,0 +1,26 @@
+const posts = [
+  {
+    id: '1',
+    content: 'Here it comes a nice post',
+    author: { id: '4', name: 'Nathan' },
+    comments: [
+      { id: '1', comment: 'Nice', author: { id: '3', name: 'Isaque' } },
+      { id: '2', comment: 'Horrible', author: { id: '1', name: 'Oleg' } },
+      {
+        id: '3',
+        comment: 'I think thats bullshit',
+        author: { id: '2', name: 'Fernando' }
+      }
+    ],
+    likes: [{ id: '3', name: 'Isaque' }, { id: '1', name: 'Oleg' }]
+  },
+  {
+    id: '2',
+    content: 'Here it comes another nice post',
+    author: { id: '3', name: 'Isaque' },
+    comments: [
+      { id: '1', comment: 'Nice', author: { id: '2', name: 'Fernando' } }
+    ],
+    likes: [{ id: '3', name: 'Isaque' }, { id: '1', name: 'Oleg' }]
+  }
+];
